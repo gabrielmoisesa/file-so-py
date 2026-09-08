e099fe02da3abe7c48ef64d8709440e5e2621e83
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-09-07 22:56:14 -0300

    feat(main): create main program terminal interactivity

--- src/main.py
+++ src/main.py
@@ -1,15 +1,47 @@
 from file_ops import create_file, read_file, rename_file, delete_file
+from sys_info import get_os_info
+
+def show_menu() -> None:
+    print("\n=== FileSO.py ===")
+    print("1. Criar arquivo com informações do SO")
+    print("2. Ler arquivo")
+    print("3. Renomear arquivo")
+    print("4. Excluir arquivo")
+    print("5. Sair")
 
 def main() -> None:
   file_path = "info_so.json"
 
-  create_file(file_path, {"SO": "Linux", "Status": "Teste"})
-  print(read_file(file_path))
+  while True:
+     show_menu()
+     selected_option = input("Escolha uma opção: ").strip()
 
-  # rename_file(file_path, "info_renomeado_so.json")
+     if selected_option == "1":
+        info = get_os_info()
+        print()
+        create_file(file_path, info)
 
-  # delete_file("info_renomeado_so.json")
-  # delete_file("info_renomeado_so.json")
+     elif selected_option == "2":
+        data = read_file(file_path)
+        if data:
+            print(f"\n======== {file_path} ========")
+            for chave, valor in data.items():
+                print(f"{chave}: {valor}")
+
+     elif selected_option == "3":
+        new_path = input("Novo nome/caminho do arquivo (ex: dados.json): ").strip()
+        if rename_file(file_path, new_path):
+           file_path = new_path
+
+     elif selected_option == "4":
+        delete_file(file_path)
+
+     elif selected_option == "5":
+        print("Encerrando o programa...")
+        break
+
+     else:
+        print("Opção inválida. Tente novamente.")
 
 if __name__ == "__main__":
     main()
