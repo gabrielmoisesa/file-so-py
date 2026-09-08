08a969004d64db0967562d36cd21e1c8bfecac79
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-09-07 22:32:25 -0300

    feat(sys_info): add more variables to info dictionary

--- src/sys_info.py
+++ src/sys_info.py
@@ -1,9 +1,15 @@
 import platform
+import socket
+import os
 
 def get_os_info() -> dict:
   info = {
     "sistema_operacional": platform.system(),
+    "release": platform.release(),
+    "versao": platform.version(),
+    "arquitetura": platform.machine(),
+    "nome_maquina": socket.gethostname(),
+    "usuario": os.getlogin(),
+    "versao_python": platform.python_version(),
   }
   return info
-
-print(get_os_info())
